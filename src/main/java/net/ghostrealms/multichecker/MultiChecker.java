1b189e22fee058b79b031ed3be53bf395f4f5e88
River Marmorstein <river@rmarmorstein.com>
2016-01-21 23:57:04 -0500

    init

---
+++ src/main/java/net/ghostrealms/multichecker/MultiChecker.java
@@ -0,0 +1,17 @@
+package net.ghostrealms.multichecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MultiChecker extends JavaPlugin {
+  
+  @Override
+  public void onEnable() {
+    
+  }
+  
+  @Override
+  public void onDisable() {
+
+  }
+
+}
